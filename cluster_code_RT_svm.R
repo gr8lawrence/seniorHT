96fbc8d54c48441ee999dac865a7aec794d4c75c
Tianyi Liu <gr8lawrence@gmail.com>
2019-01-15 11:31:14 -0500

    code for svm with rank transform

--- cluster_code_RT_svm.R
+++ cluster_code_RT_svm.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(caret)
-library(ranger) # fast implementation of random forest
 library(e1071) # svm
 set.seed(100)
 
@@ -166,14 +165,13 @@ for (i in 1:num.studies) {
 studies <- learning.df
 
 # We create an empty tibble first to hold the leanring results
-learning.results <- tibble(learning.set = rep('NA', 2*length(studies)^2), 
-                           validation.set = rep('NA', 2*length(studies)^2), 
-                           method = rep('NA', 2*length(studies)^2),
-                           gamma = rep(0, 2*length(studies)^2),
-                           cost = rep(0, 2*length(studies)^2),
-                           accuracy = rep(0, 2*length(studies)^2), 
-                           sensitivity = rep(0, 2*length(studies)^2), 
-                           specificity = rep(0, 2*length(studies)^2))
+learning.results <- tibble(learning.set = rep('NA', length(studies)^2), 
+                           validation.set = rep('NA', length(studies)^2), 
+                           gamma = rep(0, length(studies)^2),
+                           cost = rep(0, length(studies)^2),
+                           accuracy = rep(0, length(studies)^2), 
+                           sensitivity = rep(0, length(studies)^2), 
+                           specificity = rep(0, length(studies)^2))
 len <- length(studies)
 
 for (i in 1:len) {
@@ -182,37 +180,16 @@ for (i in 1:len) {
   validation.set <- studies[[i]][, 1:df.length]
   truth <- studies[[i]]$class # The truth vector
   
-  ## Run the RF (with 1500 trees) and SVM on one dataset, and validate on studies[i]
+  ## Run the SVM on one dataset, and validate on studies[i]
   for (j in 1:length(studies.min.1)) {
     learning.set <- studies.min.1[[j]]
     
-    # Random Forest
-    rf <-
-      ranger::ranger(class ~ ., data = learning.set, num.trees = 1500, probability = TRUE)
-    pred <- predict(rf, validation.set)
-    confusion.mat <- confusionMatrix(data = as.factor(ifelse(pred$predictions[,1] > 0.5, "basal", "classical")),
-                                     reference = truth)
-    accu <- confusion.mat$overall[["Accuracy"]]
-    sen <- confusion.mat$byClass[["Sensitivity"]]
-    spe <- confusion.mat$byClass[["Specificity"]]
-    learning.results[10 * (i - 1) + 2 * j - 1,] <-
-      c(
-        studies.names.min.1[j],
-        studies.names[i],
-        "random forest",
-        NaN,
-        NaN,
-        signif(accu, digits = 4),
-        signif(sen, digits = 4),
-        signif(spe, digits = 4)
-      )
-    
-    # Support Vector Machine
-    # First, we need to determine the tuning parameters so the SVM gains some sensitivity
+    # We go through parameter tuning at first to search for the optimal cost C and tuning parameter gamma for the radial basis kernel
+    # Use a grid search to find the best (C, gamma) pair
     # Cross-validation generate 40 SVMs  
     tune <- tune.svm(class ~ ., data = learning.set, 
-                     gamma = seq(1/dim(learning.set)[2], 3/dim(learning.set)[2], by = 0.5/dim(learning.set)[2]), 
-                     cost = seq(0.5, 1, by = 0.1))
+                     gamma = 10^seq(-5, 3, 2), 
+                     cost = 10^seq(-3, 5, 2)) 
     classical.basal.ratio = sum(learning.set$class == "classical")/sum(learning.set$class == "basal")
     supp.vec <-
       svm(class ~ ., data = learning.set, 
@@ -226,52 +203,26 @@ for (i in 1:len) {
     accu <- confusion.mat$overall[["Accuracy"]]
     sen <- confusion.mat$byClass[["Sensitivity"]]
     spe <- confusion.mat$byClass[["Specificity"]]
-    learning.results[10 * (i - 1) + 2 * j,] <-
+    learning.results[5 * (i - 1) + j,] <-
       c(
         studies.names.min.1[j],
         studies.names[i],
-        "SVM",
         tune$best.parameters$gamma,
         tune$best.parameters$cost,
-        signif(accu, digits = 4),
-        signif(sen, digits = 4),
-        signif(spe, digits = 4)
+        signif(accu, digits = 3),
+        signif(sen, digits = 3),
+        signif(spe, digits = 3)
       )
   }
   
-  ## Run the RF and SVM on combined datasets
+  ## Run the SVM on combined datasets
   learning.set <- studies.min.1[[1]]
   for (k in 1:(length(studies.min.1) - 1)) {
     learning.set <- rbind(learning.set, studies.min.1[[1 + k]])
   }
-  
-  # Random Forest
-  rf <-
-    ranger::ranger(class ~ ., data = learning.set, num.trees = 1500, probability = TRUE)
-  pred <- predict(rf, validation.set)
-  confusion.mat <- confusionMatrix(data = as.factor(ifelse(pred$predictions[,1] > 0.5, "basal", "classical")), 
-                                   reference = truth)
-  accu <- confusion.mat$overall[["Accuracy"]]
-  sen <- confusion.mat$byClass[["Sensitivity"]]
-  spe <- confusion.mat$byClass[["Specificity"]]
-  learning.results[10 * i - 1,] <-
-    c(
-      paste("comb. minus", studies.names[i],
-            sep = " "),
-      studies.names[i],
-      "random forest",
-      NaN,
-      NaN,
-      signif(accu, digits = 4),
-      signif(sen, digits = 4),
-      signif(spe, digits = 4)
-    )
-  
-  # Support Vector Machine
-  # We go through parameter tuning as well
   tune <- tune.svm(class ~ ., data = learning.set, 
-                   gamma = seq(1/dim(learning.set)[2], 3/dim(learning.set)[2], by = 0.5/dim(learning.set)[2]), 
-                   cost = seq(0.5, 1, by = 0.1))
+                   gamma = 10^seq(-5, 3, 2), 
+                   cost = 10^seq(-3, 5, 2)) 
   classical.basal.ratio = sum(learning.set$class == "classical")/sum(learning.set$class == "basal")
   supp.vec <-
     svm(class ~ ., data = learning.set, 
@@ -285,25 +236,22 @@ for (i in 1:len) {
   accu <- confusion.mat$overall[["Accuracy"]]
   sen <- confusion.mat$byClass[["Sensitivity"]]
   spe <- confusion.mat$byClass[["Specificity"]]
-  learning.results[10 * i,] <-
+  learning.results[5 * i,] <-
     c(
-      paste("comb. minus", studies.names[i],
-            sep = " "),
+      paste("Combined minus", studies.names[i], sep = " "),
       studies.names[i],
-      "SVM",
       tune$best.parameters$gamma,
       tune$best.parameters$cost,
-      signif(accu, digits = 4),
-      signif(sen, digits = 4),
-      signif(spe, digits = 4)
+      signif(accu, digits = 3),
+      signif(sen, digits = 3),
+      signif(spe, digits = 3)
     )
   
 }
 
 print(learning.results, n = 2*length(studies)^2)
 
-write.table(learning.results, file = "/nas/longleaf/home/tianyi96/learning_results.csv")
+write.table(learning.results, file = "/nas/longleaf/home/tianyi96/learning_results_RT_svm.csv")
 
-save(x = learning.results, file = "learning_result.Rdata")
+save(x = learning.results, file = "grid_search_result.Rdata")
 save.image()
-unlink("learning_result.Rdata")
